c45b0ad4473346c9dfc07fb13c47985a3e1b7a52
mingzi <mingzi47@qq.com>
2023-06-20 11:16:21 +0800

    add wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,88 @@
+local wezterm = require "wezterm"
+
+-- Initial startup
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
+
+return {
+  font = wezterm.font "Fira Code",
+  font_size = 16,
+
+  color_scheme = "Breeze (Gogh)",
+
+  window_background_opacity = 0.97,
+  window_padding = {
+   left = '0.1%',
+   right = '0%',
+   top = '1%',
+   bottom = '0%',
+  },
+  window_background_image_hsb = {
+   brightness = 0.8,
+   hue = 1.0,
+   saturation = 1.0
+  },
+  window_decorations = "RESIZE",
+  window_close_confirmation = "NeverPrompt",
+  initial_rows = 40,
+  initial_cols = 130,
+  tab_max_width = 20,
+  hide_tab_bar_if_only_one_tab = true,
+  --window_background_image = "/Users/mingzi/Pictures/wallpapers/wallpaper-103.png",
+  leader = {
+    key = 'p',
+    mods = 'CMD',
+    timeout_milliseconds = 3000,
+  },
+  keys = {
+    -- Pane
+    {
+      key = 'v',
+      mods = 'LEADER',
+      action = wezterm.action.SplitHorizontal {  domain = 'CurrentPaneDomain'},
+    },
+    {
+      key = 's',
+      mods = 'LEADER',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'x',
+      mods = 'LEADER',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+      key = 'q',
+      mods = 'LEADER',
+      action = wezterm.action.PaneSelect {
+        alphabet = '1234567890',
+      },
+    },
+    -- Tab
+    { -- 新建窗口
+        key = 'c',
+        mods = 'LEADER',
+        action = wezterm.action.SpawnCommandInNewTab {
+            label = 'Zsh-NewWindow',
+            args = {'/bin/zsh', '-l'}
+        }
+    },
+    {
+      key = 'p',
+      mods = 'LEADER',
+      action = wezterm.action {
+        ActivateTabRelative = -1
+      }
+    },
+    {
+      key = 'n',
+      mods = 'LEADER',
+      action = wezterm.action {
+        ActivateTabRelative = 1
+      }
+    },
+  }
+
+}
