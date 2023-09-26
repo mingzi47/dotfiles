08d3aeee5fb9db31ea18dd72335c6519836355df
mingzi <mingzi47@qq.com>
2023-09-26 14:49:58 +0800

    Tue Sep 26 14:50:09 CST 2023

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,90 +6,92 @@ wezterm.on('gui-startup', function(cmd)
   window:gui_window():maximize()
 end)
 
-return {
-  font = wezterm.font "Fira Code",
-  font_size = 16,
+local config = {}
 
-  color_scheme = "Breeze (Gogh)",
 
-  window_background_opacity = 0.95,
-  window_padding = {
-    left = '0%',
-    right = '0%',
-    top = '1%',
-    bottom = '0%',
-  },
-  window_background_image_hsb = {
-    brightness = 0.8,
-    hue = 1.0,
-    saturation = 1.0
-  },
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
-  initial_rows = 40,
-  initial_cols = 130,
-  tab_max_width = 20,
-  hide_tab_bar_if_only_one_tab = true,
-  -- background = {
-  --   {
-  --     source = {
-  --       File = "Users/mingzi/.config/wezterm/wallpapers/0.png",
-  --     },
-  --     hsb = { brightness = 0.13 },
-  --   }
-  -- },
-  leader = {
-    key = 'p',
-    mods = 'CMD',
-    timeout_milliseconds = 3000,
-  },
-  keys = {
-    -- Pane
-    {
-      key = 'v',
-      mods = 'LEADER',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 's',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'x',
-      mods = 'LEADER',
-      action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
-    {
-      key = 'q',
-      mods = 'LEADER',
-      action = wezterm.action.PaneSelect {
-        alphabet = '1234567890',
-      },
-    },
-    -- Tab
-    { -- 新建窗口
-      key = 'c',
-      mods = 'LEADER',
-      action = wezterm.action.SpawnCommandInNewTab {
-        label = 'Zsh-NewWindow',
-        args = { '/bin/zsh', '-l' }
-      }
-    },
-    {
-      key = 'p',
-      mods = 'LEADER',
-      action = wezterm.action {
-        ActivateTabRelative = -1
-      }
-    },
-    {
-      key = 'n',
-      mods = 'LEADER',
-      action = wezterm.action {
-        ActivateTabRelative = 1
-      }
-    },
-  }
+config.font = wezterm.font "Fira Code"
+config.font_size = 16
+config.color_scheme = "Breeze (Gogh)"
 
+config.window_background_opacity = 0.95
+config.window_padding = {
+  left = '0%',
+  right = '0%',
+  top = '1%',
+  bottom = '0%',
 }
+config.window_background_image_hsb = {
+  brightness = 0.8,
+  hue = 1.0,
+  saturation = 1.0
+}
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.initial_rows = 40
+config.initial_cols = 130
+config.tab_max_width = 20
+config.hide_tab_bar_if_only_one_tab = true
+-- config.background = {
+--   {
+--     source = {
+--       File = "Users/mingzi/.config/wezterm/wallpapers/0.png",
+--     },
+--     hsb = { brightness = 0.13 },
+--   }
+-- },
+config.leader = {
+  key = 'p',
+  mods = 'CMD',
+  timeout_milliseconds = 3000,
+}
+config.keys = {
+  -- Pane
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'x',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = 'q',
+    mods = 'LEADER',
+    action = wezterm.action.PaneSelect {
+      alphabet = '1234567890',
+    },
+  },
+  -- Tab
+  {   -- 新建窗口
+    key = 'c',
+    mods = 'LEADER',
+    action = wezterm.action.SpawnCommandInNewTab {
+      label = 'Zsh-NewWindow',
+      args = { '/bin/zsh', '-l' }
+    }
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = wezterm.action {
+      ActivateTabRelative = -1
+    }
+  },
+  {
+    key = 'n',
+    mods = 'LEADER',
+    action = wezterm.action {
+      ActivateTabRelative = 1
+    }
+  },
+}
+
+
+return config
