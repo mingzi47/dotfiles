071bd5e8930ff211e25446885d7400556284ab29
mingzi <1409429504@qq.com>
2023-10-03 21:20:19 +0800

    modify font in windwos

--- wezterm/configs/font.lua
+++ wezterm/configs/font.lua
@@ -1,8 +1,17 @@
 local wezterm = require("wezterm")
+local platform = require("utils.platform")()
 local config = {}
 
 config.font = wezterm.font "Fira Code"
-config.font_size = 16
+
+if platform.is_mac then
+  config.font_size = 16
+elseif platform.is_linux then
+  config.font_size = 13
+elseif platform.is_win then
+  config.font_size = 13
+end
+
 config.colors = require("colors.custom")
 
 return config
