ab91bfde40ede2291bec93248397079b74f9a6b4
mingzi <1409429504@qq.com>
2025-01-12 00:06:31 +0800

    .

--- nvim/lua/internal/init.lua
+++ nvim/lua/internal/init.lua
@@ -1,34 +1,33 @@
 local autocmd = vim.api.nvim_create_autocmd
 local ucommand = vim.api.nvim_create_user_command
 local function augroup(name)
-  return vim.api.nvim_create_augroup("mingzi_nvim_" .. name, { clear = true })
+    return vim.api.nvim_create_augroup("mingzi_nvim_" .. name, { clear = true })
 end
 
 require("internal.keymap")
 
 -- auto pairs
-autocmd({'InsertEnter', 'CmdlineEnter'},
-{
-    group = augroup('editor'),
-    once = true,
-    callback = function()
-        require 'internal.editor.pairs'
-    end,
+autocmd({ 'InsertEnter', 'CmdlineEnter' },
+    {
+        group = augroup('editor'),
+        once = true,
+        callback = function()
+            require 'internal.editor.pairs'
+        end,
 
-})
+    })
 
 
 -- buffer init
 autocmd('BufEnter',
-{
-    group = augroup('init'),
-    once = true,
-    callback = function()
-        -- theme
-        vim.cmd("colorscheme gruvbox")
-        -- statusline
-        require("internal.statusline").setup()
-        -- keymap
-    end,
-}
-)
+    {
+        group = augroup('init'),
+        once = true,
+        callback = function()
+            -- theme
+            vim.cmd("colorscheme gruvbox")
+            -- statusline
+            require("internal.statusline").setup()
+            -- keymap
+        end,
+    })
