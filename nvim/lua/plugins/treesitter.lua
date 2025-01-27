e0b1261c240ee30d29951158e737b62d6a2dc226
mingzi <1409429504@qq.com>
2025-01-27 15:53:31 +0800

    treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,54 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    init = function(plugin)
+        require("lazy.core.loader").add_to_rtp(plugin)
+        require("nvim-treesitter.query_predicates")
+    end,
+    opts = {
+        ensure_installed = {
+            "bash",
+            "diff",
+            "json",
+            "jsonc",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "latex",
+            "printf",
+            "query",
+            "regex",
+            "toml",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+            "c",
+            "rust",
+            "cpp",
+            "python",
+            "go",
+        },
+
+        sync_install = false,
+        auto_install = true,
+
+        ignore_install = {},
+        highlight = {
+            enable = true
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end
+}
