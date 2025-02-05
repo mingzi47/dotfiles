3b3901f9eebc96af073f89eecc374189ca7b855e
mingzi <1409429504@qq.com>
2025-02-05 16:11:51 +0800

    [neovim] add which-key

---
+++ nvim/lua/plugins/which_key.lua
@@ -0,0 +1,28 @@
+local M = {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        preset = "helix",
+        icons = {
+            mappings = false,
+        },
+        spec = {
+            { "<leader>f", group = "fzf" },
+            { "<leader>a", group = "action" },
+            { "<leader>b", group = "buffer" },
+            { "<leader>h", group = "help" },
+            { "<leader>t", group = "term" },
+        }
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
+
+return M
