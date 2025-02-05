6801a4836fbcf13094f8cf0ef4051a08e4a541db
mingzi <1409429504@qq.com>
2025-02-05 14:24:06 +0800

    [neovim] help gx

--- nvim/lua/internal/keymap.lua
+++ nvim/lua/internal/keymap.lua
@@ -4,8 +4,3 @@ map("n", "<leader>hm", function ()
     require("internal.tools.messages").messages()
 end, { desc = "Messages History" })
 
-
-
-map({ "n", "t", "v", "x" }, "gf", function ()
-    require("internal.tools.gf").gf()
-end, { desc = "Goto file(URL)" })
