012e7bb1f5693bc9beb2637398efb791ad88cc60
mingzi <1409429504@qq.com>
2025-03-03 15:14:39 +0800

    [neovim] add session

---
+++ nvim/lua/plugins/sessions.lua
@@ -0,0 +1,16 @@
+local M = {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    keys = {
+        { "<leader>sl", function() require("persistence").load() end,   desc = "Restore Session" },
+        { "<leader>ss", function() require("persistence").select() end, desc = "Select Session" },
+    },
+    opts = {
+        dir = vim.fn.stdpath("state") .. "/sessions/",
+        need = 0,
+        branch = true,
+    },
+}
+
+
+return M
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -44,41 +44,8 @@ return {
         highlight = {
             enable = true
         },
-        textobjects = {
-            move = {
-                enable = true,
-                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-            },
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                    ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-                },
-                selection_modes = {
-                    ['@parameter.outer'] = 'v',
-                    ['@function.outer'] = 'V',
-                    ['@class.outer'] = '<c-v>',
-                },
-            },
-        },
     },
     config = function(_, opts)
         require("nvim-treesitter.configs").setup(opts)
     end,
-    cond = function()
-        return true
-    end,
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        }
-    }
 }
